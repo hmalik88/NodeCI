d5bfcb2a483b4542c1c0aae2d6a8c3f05bcf173e
hmalik88 <hbmalik88@gmail.com>
2020-08-14 11:16:28 -0400

    removed all files that are in git ignore

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://hbm88:ZRwx0Rhvn97euD88@cluster0-shard-00-00.xhjqw.mongodb.net:27017,cluster0-shard-00-01.xhjqw.mongodb.net:27017,cluster0-shard-00-02.xhjqw.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-iofuiz-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
